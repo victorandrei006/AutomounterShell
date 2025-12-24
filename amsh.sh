c75d00fea94904b3ad39b3756622d78199d6504e
victorandrei006 <victorandrei006@gmail.com>
2025-12-24 23:35:30 +0200

    attempt commit 2

--- amsh.sh
+++ amsh.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 config="mnt.conf"
+log_file="$HOME/amsh.log"
 
 log_event() {
     local message="$1"
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - $message" >> ~/amsh.log
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $message" >> "$log_file"
+}
+check_log(){
+    if [ ! -f "$log_file"]; then
+    touch "$log_file"
+    fi
+    if [ ! -w "$log_file"]; then
+    echo "ERROR: lipsa permisiune de scriere!"
+    exit 1
+    fi
 }
 
 check_config() {
@@ -13,7 +23,7 @@ check_config() {
         return 1
     fi
 
-    if grep -q "^target_dir" "$config"; then
+    if grep -q "^$target_dir" "$config"; then
         return 0
     else
        return 1
@@ -25,7 +35,10 @@ log_event "Shell-ul a fost pornit de $USER."
 
 while true; do
     printf ">"
+    echo "1"
     read -r comanda
     [ "$comanda" == "exit" ] && break
+    [ "$comanda" == "q" ] && break
+    [ "$comanda" == "done" ] && break
     $comanda
 done
