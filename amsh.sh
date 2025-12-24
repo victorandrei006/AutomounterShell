49b901e459c5b77013bccd154710f96c10ad5240
PixelGuy245 <statescutudor61@gmail.com>
2025-12-24 22:43:06 +0200

    creare fisier mnt.conf cu mountpoint directories si functia check_config care verifica daca destinatia este in fisier

--- amsh.sh
+++ amsh.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
+config="mnt.conf"
 
 log_event() {
     local message="$1"
     echo "$(date '+%Y-%m-%d %H:%M:%S') - $message" >> ~/amsh.log
 }
 
+check_config() {
+    local target_dir="$1"
+
+    if [ ! -f "$config" ]; then
+        return 1
+    fi
+
+    if grep -q "^target_dir" "$config"; then
+        return 0
+    else
+       return 1
+    fi
+}
 
 echo "Automounter Shell pornit."
 log_event "Shell-ul a fost pornit de $USER."
