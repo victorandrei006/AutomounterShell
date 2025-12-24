1ac216567c804be9a5966605324df5b86c7e327d
Razvan12354 <razvanvoicu1001@gmail.com>
2025-12-24 21:27:59 +0200

    creare logger cu data + nume, executare comenzi

--- amsh.sh
+++ amsh.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+log_event() {
+    local message="$1"
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $message" >> ~/amsh.log
+}
+
+
+echo "Automounter Shell pornit."
+log_event "Shell-ul a fost pornit de $USER."
+
+while true; do
+    printf ">"
+    read -r comanda
+    [ "$comanda" == "exit" ] && break
+    $comanda
+done
