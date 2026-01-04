74e9de659dc4e568fa04b082d83b74a954f35ad8
Statescu Tudor <statescutudor61@gmail.com>
2026-01-04 16:32:45 +0200

    Schimat variabila target si adaugarea target_abs in caz ca target este eronat. Fixarea problemelor de logica cu check_config in while Adaugarea "check_log" si "trap on_exit..." pentru verificarea permisiunilor la deschidere si logarea la inchidere

--- amsh.sh
+++ amsh.sh
@@ -2,6 +2,9 @@
 config="mnt.conf"
 log_file="$HOME/amsh.log"
 
+check_log
+trap on_exit EXIT SIGINT SIGTERM
+
 log_event() {
     local message="$1"
     echo "$(date '+%Y-%m-%d %H:%M:%S') - $message" >> "$log_file"
@@ -57,34 +60,34 @@ while true; do
     argumente=$(echo "$linie" | cut -d' ' -f2-)
 
     if [ "$comanda" == "cd" ]; then
-        if [ "$argumente" == "cd" ] || [ -z "$argumente" ]; then
+        if [ -z "$argumente" ] || [ "$argumente" == "cd" ]; then
             target=$HOME
         else
             target=$argumente
         fi
+        
+        if builtin cd "$target" 2>/dev/null; then
+            target_abs=$(pwd)
 
-        if check_config "$target"; then
-            dispozitiv=$(grep -w "^$target" "$config" | awk '{print $2}')
+            if check_config "$target_abs"; then
+                dispozitiv=$(grep -w "^$target_abs" "$config" | awk '{print $2}')
 
-            if ! mountpoint -q "$target"; then
-                echo "Se montează $dispozitiv pe $target"
-                sudo mount "$dispozitiv" "$target"
-                if [ $? -eq 0 ]; then
-                    log_event "$dispozitiv a fost montat pe $target"
-                else
-                    log_event "Eroare: $dispozitiv nu a putut fi montat pe $target"
+                if ! mountpoint -q "$target_abs"; then
+                    echo "Se montează $dispozitiv pe $target_abs"
+                    sudo mount "$dispozitiv" "$target_abs"
+                    if [ $? -eq 0 ]; then
+                        log_event "$dispozitiv a fost montat pe $target_abs"
+                    else
+                        log_event "Eroare: $dispozitiv nu a putut fi montat pe $target_abs"
+                    fi
                 fi
+                cronos "$target_abs"
             fi
-            cronos "$target"
-        fi
-  
-    #odata ce scrii "cd" o sa dea mount la ce e in fisierul mnt.conf
-
-        builtin cd "$target" 2>/dev/null
-        if [ $? -ne 0 ]; then
+        else
             echo "Eroare: Directorul '$target' nu exista."
+        
         fi
     else
-        $linie
+        eval "$linie"
     fi
 done
